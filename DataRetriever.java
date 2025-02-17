32d7ff3d85fd38a2bab7b435fd6e0d5472ad5596
emrone123 <emrone@roehampton.ac.uk>
2025-02-17 16:04:10 +0000

    Update DataRetriever.java

--- DataRetriever.java
+++ DataRetriever.java
@@ -1,10 +1,22 @@
 package adapters;
 
 import core.SharePrice;
-
 import java.time.LocalDate;
 import java.util.List;
 
+/**
+ * The DataRetriever interface provides a method to fetch daily stock prices
+ * for a specified symbol within a given date range.
+ */
 public interface DataRetriever {
+    /**
+     * Retrieves a list of SharePrice objects representing the daily prices
+     * of the specified stock symbol between the given start and end dates.
+     *
+     * @param symbol    the stock symbol (e.g., "AAPL" for Apple Inc.)
+     * @param startDate the start date of the desired date range
+     * @param endDate   the end date of the desired date range
+     * @return a list of SharePrice objects containing the daily stock prices
+     */
     List<SharePrice> getDailyPrices(String symbol, LocalDate startDate, LocalDate endDate);
 }
